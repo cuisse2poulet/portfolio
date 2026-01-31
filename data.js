3bad1ce873ab7f9a2fc2351efe4d6f7aa25c468d
Joel Falcou <joel.falcou@lri.fr>
2026-01-31 15:48:23 +0100

    More digital content

--- data.js
+++ data.js
@@ -20,6 +20,10 @@ const subCategories = {
     { id: 'faces', label: 'Visages' },
     { id: 'jojo', label: 'JoJo\'s' },
     { id: 'archi', label: 'Architecture' }
+  ],
+  'digital': [
+    { id: 'anatomy', label: 'Etudes : Anatomie' },
+    { id: 'color', label: 'Etudes : Couleur' },
   ]
 };
 const portfolioItems = [
@@ -113,6 +117,30 @@ const portfolioItems = [
     title: 'Zéro G',
     desc: 'Etude de pose - Crayon sur papier'
   },
+  {
+    src: 'assets/charadesign/axolotl_aztec_race-v1.png',
+    category: 'charadesign',
+    title: 'Aztec Inspired Axolotl Race - 1',
+    desc: 'Projet réalisé sur Krita.'
+  },
+  {
+    src: 'assets/charadesign/axolotl_aztec_race-v2.png',
+    category: 'charadesign',
+    title: 'Aztec Inspired Axolotl Race - 2',
+    desc: 'Projet réalisé sur Krita.'
+  },
+  {
+    src: 'assets/charadesign/red_squadron.png',
+    category: 'charadesign',
+    title: 'R3D SQUADRON',
+    desc: 'Projet réalisé sur Krita.'
+  },
+  {
+    src: 'assets/charadesign/swordsman-ref.png',
+    category: 'charadesign',
+    title: 'Swordsman - Travail de référence',
+    desc: 'Projet réalisé sur Krita.'
+  },
   // --- VISAGES ---
   {
     src: 'assets/faces/faces1.jpg',
@@ -173,16 +201,130 @@ const portfolioItems = [
 
   // --- Digital ---
   {
-    src: 'assets/digital1.jpg',
+    src: 'assets/digital/anatomy/full_upper_torso.png',
     category: 'digital',
-    title: 'Affiche numérique',
-    desc: 'Projet réalisé sur Photoshop.'
+    sub: 'anatomy',
+    title: 'Full Upper Torso',
+    desc: 'Projet réalisé sur Krita.'
   },
   {
-    src: 'assets/digital2.jpg',
+    src: 'assets/digital/anatomy/random.png',
     category: 'digital',
-    title: 'Composition 3D',
-    desc: 'Rendu Blender et post-prod.'
+    sub: 'anatomy',
+    title: 'Random',
+    desc: 'Projet réalisé sur Krita.'
+  },
+  {
+    src: 'assets/digital/anatomy/shapes.png',
+    category: 'digital',
+    sub: 'anatomy',
+    title: 'Shapes',
+    desc: 'Projet réalisé sur Krita.'
+  },
+  {
+    src: 'assets/digital/anatomy/upper_boody.png',
+    category: 'digital',
+    sub: 'anatomy',
+    title: 'Upper Boody',
+    desc: 'Projet réalisé sur Krita.'
+  },
+  {
+    src: 'assets/digital/anatomy/upper_tordo.png',
+    category: 'digital',
+    sub: 'anatomy',
+    title: 'Upper Tordo',
+    desc: 'Projet réalisé sur Krita.'
+  },
+  {
+    src: 'assets/digital/color/basic_girl_first digital_drawing.png',
+    category: 'digital',
+    sub: 'color',
+    title: 'Basic Girl',
+    desc: 'Projet réalisé sur Krita.'
+  },
+  {
+    src: 'assets/digital/color/boy_in_forest.png',
+    category: 'digital',
+    sub: 'color',
+    title: 'Boy In The Forest',
+    desc: 'Projet réalisé sur Krita.'
+  },
+  {
+    src: 'assets/digital/color/dalek_art.png',
+    category: 'digital',
+    sub: 'color',
+    title: 'Dalek',
+    desc: 'Projet réalisé sur Krita.'
+  },
+  {
+    src: 'assets/digital/color/Drunk_tubies.png',
+    category: 'digital',
+    sub: 'color',
+    title: 'Drunk Tubies',
+    desc: 'Projet réalisé sur Krita.'
+  },
+  {
+    src: 'assets/digital/color/Fashion_outfit_TriangleBased.png',
+    category: 'digital',
+    sub: 'color',
+    title: 'Triangle Based Fashion Outfit',
+    desc: 'Projet réalisé sur Krita.'
+  },
+  {
+    src: 'assets/digital/color/fiin_and_huntress_drawing2.png',
+    category: 'digital',
+    sub: 'color',
+    title: 'Fiin And Huntress',
+    desc: 'Projet réalisé sur Krita.'
+  },
+  {
+    src: 'assets/digital/color/jim_the_machine_redsquadron.png',
+    category: 'digital',
+    sub: 'color',
+    title: 'Jim The Machine - Redsquadron',
+    desc: 'Projet réalisé sur Krita.'
+  },
+  {
+    src: 'assets/digital/color/lesbcouple.png',
+    category: 'digital',
+    sub: 'color',
+    title: 'Lesbian Couple',
+    desc: 'Projet réalisé sur Krita.'
+  },
+  {
+    src: 'assets/digital/color/magic_voom.png',
+    category: 'digital',
+    sub: 'color',
+    title: 'Magic Voom',
+    desc: 'Projet réalisé sur Krita.'
+  },
+  {
+    src: 'assets/digital/color/oc_chair_empty.png',
+    category: 'digital',
+    sub: 'color',
+    title: 'Vintage Leather Chair',
+    desc: 'Projet réalisé sur Krita.'
+  },
+  {
+    src: 'assets/digital/color/oc_chair1.png',
+    category: 'digital',
+    sub: 'color',
+    title: 'Soviet Girl',
+    desc: 'Projet réalisé sur Krita.'
+  },
+  {
+    src: 'assets/digital/color/oc_comu_picnic.png',
+    category: 'digital',
+    sub: 'color',
+    title: 'Picnic',
+    desc: 'Projet réalisé sur Krita.'
+  },
+  {
+    src: 'assets/digital/color/pb_marcie_final.png',
+    category: 'digital',
+    sub: 'color',
+    title: 'Princess Bubblegum & Marcelline',
+    desc: 'Projet réalisé sur Krita.'
   },
 
   // --- Photos ---
@@ -287,19 +429,13 @@ const portfolioItems = [
   // --- 3D Models (Sketchfab) ---
   {
     category: '3d',
-    title: 'Flying Appa',
-    sketchfabId: '7abd7df5f32442b38d0a4b10fa3f22cf',
-    desc: 'Modèle inspiré de la série Avatar. Animation par mes soins.'
+    title: 'New Born Strampler',
+    sketchfabId: '163fbe99020e4ffb9530ee60f626db55'
   },
   {
     category: '3d',
-    title: 'Evil Lich',
-    sketchfabId: 'bad449f651984135bdd56df323e92115'
-  },
-  {
-    category: '3d',
-    title: 'Spectre',
-    sketchfabId: '1c2b5348b574400b9a59d64276a16650'
+    title: 'Baby Strampler',
+    sketchfabId: '0cd70b544df64718b45fb7dbf8e2bc09'
   },
   {
     category: '3d',
@@ -308,12 +444,71 @@ const portfolioItems = [
   },
   {
     category: '3d',
+    title: 'Ancient Strampler',
+    sketchfabId: '42a63b3bd32249f1b9e3872ecdeae7ed'
+  },
+  {
+    category: '3d',
+    title: 'Indigo Scyphozoa',
+    sketchfabId: 'e983601f21ad435b8ce933d14b49ff86'
+  },
+
+  {
+    category: '3d',
+    title: 'Baby Indigo Salamander',
+    sketchfabId: '1292a84c96614f18907a413e8226dc2e'
+  },
+  {
+    category: '3d',
+    title: 'Indigo Salamander',
+    sketchfabId: '658b495dccf74715b3d7e80f1001925d'
+  },
+
+  {
+    category: '3d',
     title: 'Salamander Familly',
     sketchfabId: '6a31812525214e4b89dd0b33122fd766'
   },
   {
     category: '3d',
+    title: 'Evil Lich',
+    sketchfabId: 'bad449f651984135bdd56df323e92115'
+  },
+  {
+    category: '3d',
+    title: 'Striker',
+    sketchfabId: '27c47d97bb564828a42df5c72247f6dd'
+  },
+
+  {
+    category: '3d',
+    title: 'Spectre',
+    sketchfabId: '1c2b5348b574400b9a59d64276a16650'
+  },
+  {
+    category: '3d',
+    title: 'Indigo Mantis',
+    sketchfabId: '42009cbba6c74857ba15e2ddcb97d253'
+  },
+  {
+    category: '3d',
+    title: 'Silkloom',
+    sketchfabId: '279414ad66eb40f9a0b7712ab0bbd8d9'
+  },
+  {
+    category: '3d',
+    title: 'Silkloin',
+    sketchfabId: 'b3087191d7df496ba2dd1ad9bc692de0'
+  },
+  {
+    category: '3d',
     title: 'Ravaa',
     sketchfabId: '3b7ab81a9348423db3479c0c334d2231'
-  }
+  },
+  {
+    category: '3d',
+    title: 'Flying Appa',
+    sketchfabId: '7abd7df5f32442b38d0a4b10fa3f22cf',
+    desc: 'Modèle inspiré de la série Avatar. Animation par mes soins.'
+  },
 ];
